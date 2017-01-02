501ac137d8b664be77be62c0c6bb95f976470b9c
tangsttw <auop.321@gmail.com>
2017-01-02 16:19:55 +0800

    create db file

---
+++ create_db.sql
@@ -0,0 +1,13 @@
+-- create_db.sql
+CREATE TABLE items (
+	id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	name TEXT NOT NULL,
+	group_name TEXT
+);
+
+CREATE TABLE draw_histories (
+    draw_histories_id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	itemid INTEGER,
+	time DATETIME DEFAULT (datetime('now', 'localtime')),
+	FOREIGN KEY (memberid) REFERENCES items(id)
+);
